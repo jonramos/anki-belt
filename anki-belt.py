77eccddd880441d7e804a66d9af7dbd87ec1b3c2
Jonathan Ramos <jonathan_ti@outlook.com>
2015-12-28 18:53:09 -0200

    added and formated Dialog box

--- anki-belt.py
+++ anki-belt.py
@@ -20,9 +20,51 @@ class AnkiLevel:
          mw.form.menuTools.addAction(self.menuAction)
 
      def mainFunction(self):
-       showInfo("card count: %d" % mw.col.db.scalar("select count() from cards where ivl >= 20"))
-    
+       #showInfo("card count: %d" % mw.col.db.scalar("select count() from cards where ivl >= 20"))
+       
+       #Create the main Dialog Box
+       swin = QDialog(mw)
 
+       #Create a Vertical box layout
+       vl = QVBoxLayout()
+
+       #Create a form to group other elements
+       frm = QGroupBox("Settings")
+       vl.addWidget(frm)
+
+       il = QVBoxLayout()
+       fl = QHBoxLayout()
+       
+       field = QLineEdit()
+       field.setPlaceholderText("test")
+       il.addWidget(QLabel("Please write a deck name or \"all\" to all decks"))
+       fl.addWidget(field)
+
+       il.addLayout(fl)
+       frm.setLayout(il)
+
+       hl = QHBoxLayout()
+       vl.addLayout(hl)
+       submitButton = QPushButton("Generate")
+       hl.addWidget(submitButton)
+       submitButton.connect(submitButton, SIGNAL("clicked()"), swin, SLOT("accept()"))
+       closeButton = QPushButton("Close")
+       hl.addWidget(closeButton)
+       closeButton.connect(closeButton, SIGNAL("clicked()"), swin, SLOT("reject()"))
+       
+
+       #Format main Dialog Box
+       swin.setLayout(vl)
+       swin.setTabOrder(submitButton,closeButton)
+       swin.setTabOrder(closeButton,field)
+       swin.setLayout(vl)
+       swin.resize(200, 150)
+
+       if swin.exec_():
+            mw.progress.start(immediate=True)
+            if len(field.text().strip()) != 0: _pattern = field.text().lower()
+            mw.progress.finish()
+            self.win.show()
 
 if __name__ != "__main__":
     # Save a reference to the toolkit onto the mw, preventing garbage collection of PyQT objects

b39b8f74ec6e2a2778e20a94b7adc29e221049af
Jonathan Ramos <jonathan_ti@outlook.com>
2015-12-28 18:15:23 -0200

    Added Class, some imports,and modified method mainFunction

--- anki-belt.py
+++ anki-belt.py
@@ -5,15 +5,28 @@ from aqt.utils import showInfo
 # import all of the Qt GUI library
 from aqt.qt import *
 
-def mainFunction():
+from anki.hooks import addHook
+from aqt.webview import AnkiWebView
+from aqt.qt import *
 
-    showInfo("card count: %d" % mw.col.db.scalar("select count() from cards where ivl >= 20"))
+class AnkiLevel:
+  
+
+     def __init__(self, mw):
+       if mw:
+         self.menuAction = QAction("Anki Level", mw)
+         mw.connect(self.menuAction, SIGNAL("triggered()"), self.mainFunction)
+         mw.form.menuTools.addSeparator()
+         mw.form.menuTools.addAction(self.menuAction)
+
+     def mainFunction(self):
+       showInfo("card count: %d" % mw.col.db.scalar("select count() from cards where ivl >= 20"))
+    
 
 
-
-# create a new menu item based in parameter
-action = QAction("My Anki Level", mw)
-# set it to call a function when it's clicked
-mw.connect(action, SIGNAL("triggered()"), mainFunction)
-# and add it to the tools menu
-mw.form.menuTools.addAction(action)
+if __name__ != "__main__":
+    # Save a reference to the toolkit onto the mw, preventing garbage collection of PyQT objects
+    if mw: mw.ankilevel = AnkiLevel(mw)
+else:
+    print "This is a plugin for the Anki Spaced Repition learning system and cannot be run directly."
+    print "Please download Anki2 from <http://ankisrs.net/>"
